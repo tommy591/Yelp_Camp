53c57b37fd27d228a690ef697220425e478dc0de
tommy591 <tstephensonjr91@gmail.com>
2020-12-14 16:37:50 -0800

    error handling logic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const catchAsync = require("./utils/syncWrapper")
 const Campground = require("./models/campground");
 const app = express();
 const port = 27017;
@@ -20,7 +21,7 @@ db.once("open", () => {
    console.log("Dtabase connected");
 });
 
-app.engine('ejs', ejsMate)
+app.engine("ejs", ejsMate);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -36,33 +37,36 @@ app.get("/campgrounds/new", (req, res) => {
    res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-   const campground = new Campground(req.body.campground);
-   await campground.save();
-   res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post("/campgrounds",catchAsync(async (req, res, next) => {
+      const campground = new Campground(req.body.campground);
+      await campground.save();
+      res.redirect(`/campgrounds/${campground._id}`);
+}));
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id",catchAsync(async (req, res) => {
    const { id } = req.params;
    const campground = await Campground.findById(id);
    res.render("campgrounds/show", { campground });
-});
+}));
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
+app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
    const { id } = req.params;
    const campground = await Campground.findById(id);
    res.render("campgrounds/edit", { campground });
-});
-app.put("/campgrounds/:id", async (req, res) => {
+}));
+app.put("/campgrounds/:id", catchAsync(async (req, res) => {
    const { id } = req.params;
    const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
    });
    res.redirect(`/campgrounds/${campground._id}`);
-});
-app.delete("/campgrounds/:id", async (req, res) => {
+}));
+app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
    const { id } = req.params;
    await Campground.findByIdAndDelete(id);
    res.redirect("/campgrounds");
+}));
+app.use((err, req, res, next) => {
+   res.send("Oh boy something went wrong");
 });
 app.listen(port, () => console.log(`Example app listening on port 27017`));
